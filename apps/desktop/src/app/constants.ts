d6a2317610084bc59a7d57cfc101be1d855a1317
steglasaurous <me@steglasaurous.com>
2026-09-03 14:01:07 -0400

    fix: linting problem with constants

--- apps/desktop/src/app/constants.ts
+++ apps/desktop/src/app/constants.ts
@@ -1,6 +1,6 @@
 export const rendererAppPort = 4200;
 export const rendererAppName = 'client'; // options.name.split('-')[0] + '-web'
-export const electronAppName = 'Speakosaurus';
+export const electronAppName = 'Speakosaurus' as string;
 /** Previous Electron app name; used to migrate userData after the rename. */
-export const previousElectronAppName = 'speakmanager2';
+export const previousElectronAppName = 'speakmanager2' as string;
 export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
